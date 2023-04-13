1b5f1d29e4274dc2167e72198230cbe86df690d4
James Vanderhaak <jamesvanderhaak@gmail.com>
2023-04-13 19:00:15 +0930

    Initial Commit

---
+++ AnimalSounds/AnimalSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalSoundsApp.swift
+//  AnimalSounds
+//
+//  Created by James Vanderhaak on 13/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimalSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
