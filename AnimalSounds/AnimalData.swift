595b421cfeb09fc607dc2fbbb34cb32c6f10870a
vanderhaka <125519598+vanderhaka@users.noreply.github.com>
2023-04-17 16:00:07 +0930

    code merges

--- AnimalSounds/AnimalData.swift
+++ AnimalSounds/AnimalData.swift
@@ -1,17 +1,59 @@
+import Foundation
 
 let farmAnimals = AnimalCategory(name: "Farm Animals", animals: [
-   
+    Animal(name: "Cow", thumbnail: "cowThumbnail", imageSoundMap: ["cow1": "cowSound1", "cow2": "cowSound2"], isPremium: false),
+
+    Animal(name: "Dog", thumbnail: "dogThumbnail", imageSoundMap: ["dog1": "dogSound1", "dog2": "dogSound2"], isPremium: false),
+    
+    Animal(name: "Horse", thumbnail: "horseThumbnail", imageSoundMap: ["horse1": "horseSound1", "horse2": "horseSound2"], isPremium: false),
+
+    Animal(name: "Sheep", thumbnail: "sheepThumbnail", imageSoundMap: ["sheep1": "sheepSound1", "sheep2": "sheepSound2"], isPremium: false),
+
+    Animal(name: "Pig", thumbnail: "pigThumbnail", imageSoundMap: ["pig1": "pigSound1", "pig2": "pigSound2"], isPremium: false),
+
+    Animal(name: "Goat", thumbnail: "goatThumbnail", imageSoundMap: ["goat1": "goatSound1", "goat2": "goatSound2"], isPremium: false),
+
+    Animal(name: "Chicken", thumbnail: "chickenThumbnail", imageSoundMap: ["chicken1": "chickenSound1", "chicken2": "chickenSound2"], isPremium: false),
+
+    Animal(name: "Rooster", thumbnail: "roosterThumbnail", imageSoundMap: ["rooster1": "roosterSound1", "rooster2": "roosterSound2"], isPremium: false),
+
+    Animal(name: "Duck", thumbnail: "duckThumbnail", imageSoundMap: ["duck1": "duckSound1", "duck2": "duckSound2"], isPremium: false),
+
+    Animal(name: "Goose", thumbnail: "gooseThumbnail", imageSoundMap: ["goose1": "gooseSound1", "goose2": "gooseSound2"], isPremium: false),
+
+    Animal(name: "Turkey", thumbnail: "turkeyThumbnail", imageSoundMap: ["turkey1": "turkeySound1", "turkey2": "turkeySound2"], isPremium: false),
+
+    Animal(name: "Cat", thumbnail: "catThumbnail", imageSoundMap: ["cat1": "catSound1", "cat2": "catSound2"], isPremium: false)
+    
 ])
 
+let wildAnimals = AnimalCategory(name: "Wild Animals", animals: [
+    Animal(name: "Lion", thumbnail: "lionThumbnail", imageSoundMap: ["lion1": "lionSound1", "lion2": "lionSound2"], isPremium: true),
+   
+    Animal(name: "Tiger", thumbnail: "tigerThumbnail", imageSoundMap: ["tiger1": "tigerSound1", "tiger2": "tigerSound2"], isPremium: false),
 
+    Animal(name: "Elephant", thumbnail: "elephantThumbnail", imageSoundMap: ["elephant1": "elephantSound1", "elephant2": "elephantSound2"], isPremium: false),
 
-let animalCategories = [farmAnimals, wildAnimals]
+    Animal(name: "Eagle", thumbnail: "eagleThumbnail", imageSoundMap: ["eagle1": "eagleSound1", "eagle2": "eagleSound2"], isPremium: false),
 
+    Animal(name: "Gorilla", thumbnail: "gorillaThumbnail", imageSoundMap: ["gorilla1": "gorillaSound1", "gorilla2": "gorillaSound2"], isPremium: false),
 
+    Animal(name: "Chimpanzee", thumbnail: "chimpanzeeThumbnail", imageSoundMap: ["chimpanzee1": "chimpanzeeSound1", "chimpanzee2": "chimpanzeeSound2"], isPremium: false),
 
+    Animal(name: "Orangutan", thumbnail: "orangutanThumbnail", imageSoundMap: ["orangutan1": "orangutanSound1", "orangutan2": "orangutanSound2"], isPremium: false),
 
+    Animal(name: "Cheetah", thumbnail: "cheetahThumbnail", imageSoundMap: ["cheetah1": "cheetahSound1", "cheetah2": "cheetahSound2"], isPremium: false),
 
+    Animal(name: "Jaguar", thumbnail: "jaguarThumbnail", imageSoundMap: ["jaguar1": "jaguarSound1", "jaguar2": "jaguarSound2"], isPremium: false),
 
+    Animal(name: "Penguin", thumbnail: "penguinThumbnail", imageSoundMap: ["penguin1": "penguinSound1", "penguin2": "penguinSound2"], isPremium: false),
 
+    Animal(name: "Toucan", thumbnail: "toucanThumbnail", imageSoundMap: ["toucan1": "toucanSound1", "toucan2": "toucanSound2"], isPremium: false),
 
+    Animal(name: "Zebra", thumbnail: "zebraThumbnail", imageSoundMap: ["zebra1": "zebraSound1", "zebra2": "zebraSound2"], isPremium: false),
 
+    Animal(name: "Frog", thumbnail: "frogThumbnail", imageSoundMap: ["frog1": "frogSound1", "frog2": "frogSound2"], isPremium: false),
+    
+
+    // add more wild animals here
+])
--- AnimalSounds/AnimalGridView.swift
+++ AnimalSounds/AnimalGridView.swift
@@ -16,22 +16,42 @@ struct AnimalGridView: View {
                     GridItem(.flexible(), spacing: 30)
                 ], spacing: 30) {
                     ForEach(animals) { animal in
-                        AnimalThumbnailView(
-                            animal: animal,
-                            showPopup: $showPopup,
-                            selectedAnimal: $selectedAnimal,
-                            selectedImageName: $selectedImageName
-                        )
-                            .onTapGesture {
-                                selectedAnimal = animal
-                                selectedImageName = animal.imageSoundMap.keys.randomElement()!
-                                showPopup = true
+                        ZStack {
+                            Button(action: {
+                                if !animal.isPremium || hasAccessToPremiumContent() {
+                                    selectedAnimal = animal
+                                    let imageNames = Array(animal.imageSoundMap.keys)
+                                    let randomIndex = Int.random(in: 0..<imageNames.count)
+                                    selectedImageName = imageNames[randomIndex]
+                                    showPopup.toggle()
+                                }
+                            }) {
+                                Image(animal.thumbnail)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: UIScreen.main.bounds.width * 1 / 2, height: UIScreen.main.bounds.width * 1 / 2)
+                                    .clipped()
+                                    .cornerRadius(10)
+                                    .padding(.horizontal, 4)
+                                    .padding(.vertical, 4)
                             }
+                            .buttonStyle(PlainButtonStyle())
+                            .accessibilityLabel("Select \(animal.name)")
+                            .accessibilityHint("Double-tap to see more information and hear the sound of the \(animal.name).")
+                            .disabled(animal.isPremium && !hasAccessToPremiumContent())
+
+                            if animal.isPremium && !hasAccessToPremiumContent() {
+                                Image(systemName: "lock.fill")
+                                    .resizable()
+                                    .frame(width: 44, height: 44)
+                                    .foregroundColor(.gray)
+                            }
+                        }
                     }
                 }
                 .padding(40)
             }
-            .padding(.top, 50) // Add 100 pixels padding from the top
+            .padding(.top, 50)
             .background(
                 Image("cloudySky")
                     .resizable()
@@ -50,7 +70,12 @@ struct AnimalGridView: View {
             .padding(.top, 20)
 
         }
-        .navigationBarHidden(true) // Hide the default navigation bar
+        .navigationBarHidden(true)
+    }
+    
+    func hasAccessToPremiumContent() -> Bool {
+        // Add your subscription check logic here.
+        return false
     }
 }
 
