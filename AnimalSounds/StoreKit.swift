3900f257c65caa6c991bbf1b794f4c054f7753ae
vanderhaka <jamesvanderhaak@gmail.com>
2023-05-03 07:31:11 +0930

    StoreKit file now contains subscription info

--- AnimalSounds/StoreKit.swift
+++ AnimalSounds/StoreKit.swift
@@ -1,5 +1,49 @@
-import Foundation
+import SwiftUI
 import StoreKit
+
+struct SubscriptionView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var isSubscribed = false
+    
+    @ObservedObject var storeManager = StoreManager()
+    
+    var body: some View {
+        VStack {
+            Text("Choose a Subscription Plan")
+                .font(.title)
+                .bold()
+                .padding(.top)
+            
+            ForEach(storeManager.products, id: \.self) { product in
+                Button(action: {
+                    if isSubscribed {
+                        presentationMode.wrappedValue.dismiss()
+                        return
+                    }
+                    
+                    storeManager.buyProduct(product)
+                }) {
+                    VStack {
+                        Text(product.localizedTitle)
+                            .font(.headline)
+                        Text(product.localizedDescription)
+                        Text(product.priceString())
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+                .disabled(isSubscribed)
+                .padding(.bottom)
+            }
+        }
+        .padding()
+        .onAppear {
+            storeManager.fetchProducts()
+        }
+    }
+}
 
 class StoreManager: NSObject, ObservableObject, SKPaymentTransactionObserver, SKProductsRequestDelegate {
     @Published var products: [SKProduct] = []
@@ -8,24 +52,24 @@ class StoreManager: NSObject, ObservableObject, SKPaymentTransactionObserver, SK
         "ASFS2023",
         "ASHS2023",
     ]
-    
+
     override init() {
         super.init()
         SKPaymentQueue.default().add(self)
     }
-    
+
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
-            DispatchQueue.main.async {
-                self.products = response.products
-            }
+        DispatchQueue.main.async {
+            self.products = response.products
         }
+    }
 
     func fetchProducts() {
         let request = SKProductsRequest(productIdentifiers: productIdentifiers)
         request.delegate = self
         request.start()
     }
-    
+
     func buyProduct(_ product: SKProduct) {
         let payment = SKPayment(product: product)
         SKPaymentQueue.default().add(payment)
@@ -34,7 +78,7 @@ class StoreManager: NSObject, ObservableObject, SKPaymentTransactionObserver, SK
     func restorePurchases() {
         SKPaymentQueue.default().restoreCompletedTransactions()
     }
-    
+
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
         for transaction in transactions {
             switch transaction.transactionState {
@@ -58,4 +102,3 @@ extension SKProduct {
         return formatter.string(from: self.price) ?? "N/A"
     }
 }
-
